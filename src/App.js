b35c6481645d00cb64479e553578b7bf77294bdd
Pankaj68 <ppankaj.parmar@gmail.com>
2025-10-16 01:44:46 +0530

    Phase-6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [showAR, setShowAR] = useState(false);
 
   // 8th Wall AR experience URL
-  const eighthWallUrl = 'https://autoviz.8thwall.app/wheel-ar-update/';
+  const eighthWallUrl = 'https://old.autoviz.io/ar.php#products?id=Fikse--20272&md=17&mw=10.5&mn=20272_1704512181.glb&mi=';
 
   const handleViewAR = () => {
     setShowAR(true);
